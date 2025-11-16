7e22e0cd93bb29520cfff5ac6bc556744671d093
Gautham Joshy <gauthamjoshy32@gmail.com>
2025-11-16 19:35:28 +0530

    initial commit

--- src/modules/common/pages/Home.jsx
+++ src/modules/common/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-import { Carousel } from "flowbite-react";
+
 
 function Home() {
   return (
@@ -19,19 +19,7 @@ function Home() {
         </div>
 
         <div>
-          <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
-            <Carousel>
-              <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-                Slide 1
-              </div>
-              <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-                Slide 2
-              </div>
-              <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-                Slide 3
-              </div>
-            </Carousel>
-          </div>
+          
         </div>
 
       </div>
